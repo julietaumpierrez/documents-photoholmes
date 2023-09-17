ade50b8a94f52e43314d53d1a0cc317a7dfe4f0a
Julian O'Flaherty <julianoflaherty13@gmail.com>
2023-09-17 11:09:15 -0300

    fixed default configgs

--- src/photoholmes/models/naive/method.py
+++ src/photoholmes/models/naive/method.py
@@ -1,12 +1,14 @@
+from typing import Dict, Optional
+
 import numpy as np
 
 from photoholmes.models.base import BaseMethod
+from photoholmes.utils.generic import load_yaml
 
 
 class Naive(BaseMethod):
-    '''A random method to test the program structure
-    '''
-    DEFAULT_CONFIG_PATH = 'photoholmes/models/naive/config.yaml'
+    """A random method to test the program structure"""
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
 
@@ -14,3 +16,15 @@ class Naive(BaseMethod):
         """Predicts masks from a list of images."""
         shape = image.shape[:2] if image.ndim > 2 else image.shape
         return np.random.normal(0.5, 2, size=shape)
+
+    @classmethod
+    def from_config(cls, config: Optional[str | Dict[str, str]]):
+        if isinstance(config, str):
+            config = load_yaml(config)
+
+        if config is None:
+            config = {}
+
+        threshold = config.get("threshold", 0.5)
+
+        return cls(threshold=threshold)
