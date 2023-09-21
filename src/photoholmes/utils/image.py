c135eaf0e2c2372a6abb58ec1d1799b07a3c7d9e
Rodrigo Paganini <rpaganini@dsensetech.com>
2023-09-21 10:36:13 -0300

    First double calculation. Results differ

--- src/photoholmes/utils/image.py
+++ src/photoholmes/utils/image.py
@@ -3,8 +3,10 @@ from dataclasses import dataclass
 from typing import Optional, Union
 
 import cv2 as cv
+import jpegio
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.fftpack import dct, idct
 
 IMG_FOLDER_PATH = "test_images/images/"
 
@@ -42,6 +44,90 @@ def read_mask(mask_path):
     mask = cv.imread(mask_path)
     mask = cv.cvtColor(mask, cv.COLOR_BGR2GRAY)
     return mask > mask.max() / 2
+
+
+def read_as_jpeg(image_path: str) -> np.ndarray:
+    """Reads image from path and returns DCT coefficient matrix for each channel.
+    If image is in jpeg format, it decodes the DCT stream and returns it.
+    Otherwise, the DCT stream is calculated."""
+    if image_path[-4:] == ".jpg" or image_path[-5:] == ".jpeg":
+        return _read_jpeg(image_path)
+    else:
+        raise NotImplementedError()
+
+
+def _read_jpeg(image_path: str) -> np.ndarray:
+    """
+    :param im_path: JPEG image path
+    :return: DCT_coef (Y,Cb,Cr)
+    """
+    assert image_path[-4:] == ".jpg" or image_path[-5:] == ".jpeg"
+
+    jpeg = jpegio.read(str(image_path))
+    num_channels = len(jpeg.coef_arrays)
+
+    # determine which axes to up-sample
+    # ci = jpeg.
+    ci = jpeg.comp_info
+    need_scale = [
+        [ci[i].v_samp_factor, ci[i].h_samp_factor] for i in range(num_channels)
+    ]
+    if num_channels == 3:
+        if ci[0].v_samp_factor == ci[1].v_samp_factor == ci[2].v_samp_factor:
+            need_scale[0][0] = need_scale[1][0] = need_scale[2][0] = 2
+        if ci[0].h_samp_factor == ci[1].h_samp_factor == ci[2].h_samp_factor:
+            need_scale[0][1] = need_scale[1][1] = need_scale[2][1] = 2
+    else:
+        need_scale[0][0] = 2
+        need_scale[0][1] = 2
+
+    # up-sample DCT coefficients to match image size
+    DCT_coef = np.empty((num_channels, *jpeg.coef_arrays[0].shape))
+
+    for i in range(num_channels):
+        r, c = jpeg.coef_arrays[i].shape
+        coef_view = (
+            jpeg.coef_arrays[i].reshape(r // 8, 8, c // 8, 8).transpose(0, 2, 1, 3)
+        )
+        # case 1: row scale (O) and col scale (O)
+        if need_scale[i][0] == 1 and need_scale[i][1] == 1:
+            out_arr = np.zeros((r * 2, c * 2))
+            out_view = out_arr.reshape(r * 2 // 8, 8, c * 2 // 8, 8).transpose(
+                0, 2, 1, 3
+            )
+            out_view[::2, ::2, :, :] = coef_view[:, :, :, :]
+            out_view[1::2, ::2, :, :] = coef_view[:, :, :, :]
+            out_view[::2, 1::2, :, :] = coef_view[:, :, :, :]
+            out_view[1::2, 1::2, :, :] = coef_view[:, :, :, :]
+
+        # case 2: row scale (O) and col scale (X)
+        elif need_scale[i][0] == 1 and need_scale[i][1] == 2:
+            out_arr = np.zeros((r * 2, c))
+            # DCT_coef.append(out_arr)
+            DCT_coef[i] = out_arr
+            out_view = out_arr.reshape(r * 2 // 8, 8, c // 8, 8).transpose(0, 2, 1, 3)
+            out_view[::2, :, :, :] = coef_view[:, :, :, :]
+            out_view[1::2, :, :, :] = coef_view[:, :, :, :]
+
+        # case 3: row scale (X) and col scale (O)
+        elif need_scale[i][0] == 2 and need_scale[i][1] == 1:
+            out_arr = np.zeros((r, c * 2))
+            out_view = out_arr.reshape(r // 8, 8, c * 2 // 8, 8).transpose(0, 2, 1, 3)
+            out_view[:, ::2, :, :] = coef_view[:, :, :, :]
+            out_view[:, 1::2, :, :] = coef_view[:, :, :, :]
+
+        # case 4: row scale (X) and col scale (X)
+        elif need_scale[i][0] == 2 and need_scale[i][1] == 2:
+            out_arr = np.zeros((r, c))
+            out_view = out_arr.reshape(r // 8, 8, c // 8, 8).transpose(0, 2, 1, 3)
+            out_view[:, :, :, :] = coef_view[:, :, :, :]
+
+        else:
+            raise KeyError("Something wrong here.")
+
+        DCT_coef[i] = out_arr
+
+    return DCT_coef
 
 
 @dataclass
