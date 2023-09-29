84c63c63f321812cb73a1d16c777e5b4b319fa15
Julian O'Flaherty <julianoflaherty13@gmail.com>
2023-09-28 21:03:46 -0300

    end to end research

--- research/splicebuster/20230917_splicebuster_end_to_end.py
+++ research/splicebuster/20230917_splicebuster_end_to_end.py
@@ -9,12 +9,15 @@ from photoholmes.models.splicebuster.method import Splicebuster
 
 if "research" in os.path.abspath("."):
     os.chdir("../../")
+
+    get_ipython().run_like_magic("load_ext autoreload")
+    get_ipython().run_like_magic("autoreload 2")
 # %%
 image = Image.open("data/img00.png").convert("L")
 np_image = np.array(image)
 
 # %%
-sp = Splicebuster(stride=64)
+sp = Splicebuster(stride=8)
 
 # %%
 heatmap = sp.predict(np_image)
@@ -22,3 +25,5 @@ heatmap = sp.predict(np_image)
 plt.imshow(heatmap[0])
 plt.figure()
 plt.imshow(heatmap[1])
+
+# %%
