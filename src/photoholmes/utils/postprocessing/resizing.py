63f74ded7d36771a7bfa1b35db72dd31553cd475
Julian O'Flaherty <julianoflaherty13@gmail.com>
2023-10-14 18:49:34 -0300

    rescaled mask to match original image size

---
+++ src/photoholmes/utils/postprocessing/resizing.py
@@ -0,0 +1,27 @@
+from typing import Literal, Tuple, Union
+
+import numpy as np
+from numpy.typing import NDArray
+from scipy.interpolate import RegularGridInterpolator
+
+
+def upscale_mask(
+    coords: Tuple[NDArray, NDArray],
+    mask: NDArray,
+    target_size: Tuple[int, int],
+    method: Literal[
+        "linear", "nearest", "slinear", "cubic", "quintic", "pchip"
+    ] = "linear",
+    fill_value: Union[int, float] = 0,
+) -> NDArray:
+    X, Y = target_size
+    interpolator = RegularGridInterpolator(
+        coords, mask, method=method, bounds_error=False, fill_value=fill_value
+    )
+    target_coords = np.asarray(
+        np.meshgrid(
+            np.arange(0, X),
+            np.arange(0, Y),
+        )
+    )
+    return interpolator(target_coords.reshape(2, -1).T).reshape(Y, X).T
