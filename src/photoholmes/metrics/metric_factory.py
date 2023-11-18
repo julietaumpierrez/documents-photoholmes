8a661fd41f7f384c80103095a6058ad3e3a9ecb4
JPSoteloSilva <JPSoteloSilva@github.com>
2023-11-18 10:46:32 -0300

    MetricFactory adapted to new format

--- src/photoholmes/metrics/metric_factory.py
+++ src/photoholmes/metrics/metric_factory.py
@@ -1,68 +1,48 @@
-from enum import Enum, unique
+from typing import Union
 
-from .AUROC import AUROC
-from .FPR import FPR
-from .IoU import IoU
-from .MAP import MAP
-from .MCC import MCC
-from .Precision import Precision
-from .ROC import ROC
-from .TPR import TPR
-
-
-@unique
-class MetricType(Enum):
-    AUROC = 0
-    FPR = 1
-    IoU = 2
-    MAP = 3
-    MCC = 4
-    Precision = 5
-    ROC = 6
-    TPR = 7
-
-
-def string_to_metric_type(metric_name: str) -> MetricType:
-    if metric_name == "auroc":
-        return MetricType.AUROC
-    elif metric_name == "fpr":
-        return MetricType.FPR
-    elif metric_name == "iou":
-        return MetricType.IoU
-    elif metric_name == "map":
-        return MetricType.MAP
-    elif metric_name == "mcc":
-        return MetricType.MCC
-    elif metric_name == "precision":
-        return MetricType.Precision
-    elif metric_name == "roc":
-        return MetricType.ROC
-    elif metric_name == "tpr":
-        return MetricType.TPR
-    else:
-        raise Exception("Metric Type not implemented yet.")
+from photoholmes.metrics.base import BaseMetric
+from photoholmes.metrics.registry import MetricName
 
 
 class MetricFactory:
     @staticmethod
-    def create(metric_name: str):
+    def load(metric_name: Union[str, MetricName]) -> BaseMetric:
         """Instantiates a metric corresponding to the name passed."""
-        metric_type = string_to_metric_type(metric_name)
-        if metric_type == MetricType.AUROC:
-            return AUROC()
-        elif metric_type == MetricType.FPR:
-            return FPR()
-        elif metric_type == MetricType.IoU:
-            return IoU()
-        elif metric_type == MetricType.MAP:
-            return MAP()
-        elif metric_type == MetricType.MCC:
-            return MCC()
-        elif metric_type == MetricType.Precision:
-            return Precision()
-        elif metric_type == MetricType.ROC:
-            return ROC()
-        elif metric_type == MetricType.TPR:
-            return TPR()
-        else:
-            raise Exception("Selected metric_name is not defined")
+        if isinstance(metric_name, str):
+            metric_name = MetricName(metric_name.lower())
+
+        match metric_name:
+            case MetricName.AUROC:
+                from photoholmes.metrics.AUROC import AUROC
+
+                return AUROC()
+            case MetricName.FPR:
+                from photoholmes.metrics.FPR import FPR
+
+                return FPR()
+            case MetricName.IoU:
+                from photoholmes.metrics.IoU import IoU
+
+                return IoU()
+            case MetricName.MAP:
+                from photoholmes.metrics.MAP import MAP
+
+                return MAP()
+            case MetricName.MCC:
+                from photoholmes.metrics.MCC import MCC
+
+                return MCC()
+            case MetricName.Precision:
+                from photoholmes.metrics.Precision import Precision
+
+                return Precision()
+            case MetricName.ROC:
+                from photoholmes.metrics.ROC import ROC
+
+                return ROC()
+            case MetricName.TPR:
+                from photoholmes.metrics.TPR import TPR
+
+                return TPR()
+            case _:
+                raise NotImplementedError(f"Metric '{metric_name}' is not implemented.")
