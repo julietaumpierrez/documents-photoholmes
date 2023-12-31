f27af45493b76a942d1fc45a70eefa4b4c5a004d
JPSoteloSilva <JPSoteloSilva@github.com>
2023-12-31 16:24:03 -0300

    Research para el nuevo benchmark

--- research/factories/20231225_method_device.py
+++ research/factories/20231225_method_device.py
@@ -1,6 +1,8 @@
 # %%
 import os
 
+from torchmetrics import Metric
+
 if "research" in os.path.abspath("."):
     os.chdir("../../")
 
@@ -11,8 +13,31 @@ if "research" in os.path.abspath("."):
 from photoholmes.methods.method_factory import MethodFactory
 
 # %%
-method, preprocessing = MethodFactory.load("exif_as_language", device="cuda")
+method, preprocessing = MethodFactory.load("dq", device="cpu")
 # %%
 method.model_to_device()
 
+# %%
+method.device
+# %%
+method.name
+
+method.device
+# %%
+from photoholmes.datasets.dataset_factory import DatasetFactory
+
+# %%
+dataset = DatasetFactory.load("columbia", "data/columbia")
+# %%
+dataset.class
+# %%
+from photoholmes.metrics.metric_factory import MetricFactory
+
+# %%
+metric = MetricFactory.load(["precision","FPR"])
+# %%
+metric._get_name()
+# %%
+for met in metric:
+    print(met)
 # %%
