41142fdf6fc0ea80e08a109d2dd102f3a3682a10
Juan Pablo Sotelo Silva <jpsotelosilva@gmail.com>
2024-01-20 18:11:29 -0300

    Actualizacion del research para tener en cuenta si tiene que usar tampered o no

--- research/benchmark/20240115_run_all_datasets.py
+++ research/benchmark/20240115_run_all_datasets.py
@@ -1,7 +1,10 @@
+# $$
 import os
 
 if "research" in os.path.abspath("."):
     os.chdir("../../")
+
+# %%
 from photoholmes.benchmark.model import Benchmark
 from photoholmes.datasets.dataset_factory import DatasetFactory
 from photoholmes.methods.method_factory import MethodFactory
@@ -16,20 +19,17 @@ method, preprocessing = MethodFactory.load(
 )
 base_path = "/Users/sote/Desktop/data/datasets/"
 dataset_dict = {
-    # "dso1": f"{base_path}DSO-1/tifs-database",
-    "columbia": f"{base_path}COLUMBIA", # Se rompe
-    "coverage": f"{base_path}COVERAGE",
+    "dso1": f"{base_path}DSO-1/tifs-database",
+    "columbia": f"{base_path}COLUMBIA",
     "casia1_copy_move": f"{base_path}CASIA_1",
     "casia1_splicing": f"{base_path}CASIA_1",
     "realistic_tampering": f"{base_path}realistic_tampering",
-    
     "autosplice_100": f"{base_path}AutoSplice",
     "autosplice_90": f"{base_path}AutoSplice",
     "autosplice_75": f"{base_path}AutoSplice",
-    
     # "trace_noise_exo": f"{base_path}minitrace",
     # "trace_noise_endo": f"{base_path}minitrace",
-    "trace_cfa_alg_exo": f"{base_path}minitrace", # Se rompe
+    "trace_cfa_alg_exo": f"{base_path}minitrace",
     "trace_cfa_alg_endo": f"{base_path}minitrace",
     "trace_cfa_grid_exo": f"{base_path}minitrace",
     "trace_cfa_grid_endo": f"{base_path}minitrace",
@@ -39,46 +39,59 @@ dataset_dict = {
     "trace_jpeg_quality_endo": f"{base_path}minitrace",
     "trace_hybrid_exo": f"{base_path}minitrace",
     "trace_hybrid_endo": f"{base_path}minitrace",
-    
-    
+    "coverage": f"{base_path}COVERAGE",
 }
 
+
 for dataset_name, dataset_path in dataset_dict.items():
-    tampered_only = True
-    dataset = DatasetFactory.load(
-        dataset_name=dataset_name,
-        dataset_dir=dataset_path,
-        tampered_only=tampered_only,
-        transform=preprocessing,
-    )
+    if dataset_name in ["columbia", "casia1_copy_move", "casia1_splicing"]:
+        tampered = [False, True]
+    elif dataset_name in [
+        "coverage",
+        "realistic_tampering",
+        "autosplice_100",
+        "autosplice_90",
+        "autosplice_75",
+        "dso1",
+    ]:
+        tampered = [False]
+    else:
+        tampered = [True]
+    for tampered_only in tampered:
+        dataset = DatasetFactory.load(
+            dataset_name=dataset_name,
+            dataset_dir=dataset_path,
+            tampered_only=tampered_only,
+            transform=preprocessing,
+        )
 
-    metrics = MetricFactory.load(
-        [
-            "auroc",
-            "fpr",
-            "iou",
-            "mcc",
-            "precision",
-            "roc",
-            "tpr",
-            "f1_weighted_v1",
-            "iou_weighted_v1",
-            "mcc_weighted_v1",
-            "f1_weighted_v2",
-            "iou_weighted_v2",
-            "mcc_weighted_v2",
-            "f1",
-        ]
-    )
+        metrics = MetricFactory.load(
+            [
+                "auroc",
+                "fpr",
+                "iou",
+                "mcc",
+                "precision",
+                "roc",
+                "tpr",
+                "f1_weighted_v1",
+                "iou_weighted_v1",
+                "mcc_weighted_v1",
+                "f1_weighted_v2",
+                "iou_weighted_v2",
+                "mcc_weighted_v2",
+                "f1",
+            ]
+        )
 
-    benchmark = Benchmark(
-        save_output=True,
-        output_path="output/",
-        device=device,
-    )
+        benchmark = Benchmark(
+            save_output=True,
+            output_path="output/",
+            device=device,
+        )
 
-    benchmark.run(
-        method=method,
-        dataset=dataset,
-        metrics=metrics,
-    )
+        benchmark.run(
+            method=method,
+            dataset=dataset,
+            metrics=metrics,
+        )
