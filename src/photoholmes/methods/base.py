163cc1f537ffc1b517cf90e5fa0ea5d1d00871d9
Julieta Umpierrez <julietaumpierrez@192.168.1.4>
2024-01-23 20:49:44 -0300

    Commented line self.method_to_device in init of base model that caused init problems

--- src/photoholmes/methods/base.py
+++ src/photoholmes/methods/base.py
@@ -29,6 +29,7 @@ class BaseMethod(ABC):
         """
         self.threshold = threshold
         self.device = torch.device(device)
+        # self.method_to_device(device)
 
     @abstractmethod
     def predict(self, image: T) -> Dict[str, Any]:
