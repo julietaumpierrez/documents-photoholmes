b7f69d78e1a6444e0b84feacb8dbbdb99851720d
JPSoteloSilva <jpsotelosilva@gmail.com>
2024-03-08 00:42:28 -0300

    Path como posibilidad al load_yaml

--- src/photoholmes/utils/generic.py
+++ src/photoholmes/utils/generic.py
@@ -1,9 +1,10 @@
+from pathlib import Path
 from typing import Any, Dict
 
 import yaml
 
 
-def load_yaml(yaml_path: str) -> Dict[str, Any]:
+def load_yaml(yaml_path: str | Path) -> Dict[str, Any]:
     with open(yaml_path, "r") as file:
         data = yaml.safe_load(file)
     return data
