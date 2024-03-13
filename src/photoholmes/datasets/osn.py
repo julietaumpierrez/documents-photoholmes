7b5952766c758e9c638f2db10f0d6e58f084df33
Juan Pablo Sotelo Silva <jpsotelosilva@gmail.com>
2024-03-13 18:30:03 -0300

    Casia refactoreado y casia osn refactoreado

--- src/photoholmes/datasets/osn.py
+++ src/photoholmes/datasets/osn.py
@@ -1,56 +1,43 @@
-import glob
-import os
-from typing import List, Tuple
-
-from torch import Tensor
-
-from .base import BaseDataset
+from .casia1 import CasiaBaseDataset
 
 
-class OSNDataset(BaseDataset):
+class Casia1SplicingOSNDataset(CasiaBaseDataset):
     """
+    Class for the CASIA 1.0 Splicing subset with Online Social Networks (OSN)
+    modifications.
+
     Directory structure:
-    img_dir (ImageForgeriesOSN_Dataset)
-    ├── [base directory]
-    │   └── ...[img in PNG/JPG/TIF]
-    ├── [base_directory + social_network_modification]
-    │   └── ...[img in PNG/JPG/TIF]
-    ├── [base_directory_masks]
-        └── ...[masks in PNG/JPG/TIF]
+    img_dir (OSN dataset)
+    ├── Casia_Facebook
+    │   ├── CM
+    │   │   └── [Copy Move images in jpg]
+    │   ├── Sp
+    │   │   └── [Spliced images in jpg]
+    │   ├── CASIA_GT
+    │       └── [Copy Move and Spliced masks in png]
+    └── Possibly more files
     """
 
-    BASE_FOLDERS = ["CASIA", "Columbia", "DSO"]
-    MASKS_DIR_TAG = "_GT"
-    MASKS_FILENAME_TAG = "_gt"
-    IMAGE_EXTENSIONS = [".tif", ".jpg", ".jpeg", ".png"]
-    MASK_EXTENSION = ".png"
-    TAMPERED_INTENSITY_THRESHOLD = 125
+    IMAGES_TAMPERED_DIR = "Casia_Facebook/Sp"
+    MASK_TAMPERED_DIR = "Casia_Facebook/CASIA_GT"
 
-    def _get_paths(
-        self, img_dir: str, tampered_only: bool
-    ) -> Tuple[List[str], List[str] | List[str | None]]:
-        image_paths = []
-        mask_paths = []
-        for base_dir in self.BASE_FOLDERS:
-            for folder in glob.glob(os.path.join(img_dir, base_dir + "*")):
-                if folder != os.path.join(img_dir, base_dir) + self.MASKS_DIR_TAG:
-                    folder_im_paths = [
-                        os.path.join(folder, filename)
-                        for filename in os.listdir(folder)
-                    ]
-                    image_paths += folder_im_paths
-        mask_paths = [self._get_mask_path(image_path) for image_path in image_paths]
-        return image_paths, mask_paths
 
-    def _get_mask_path(self, image_path: str) -> str:
-        img_path_list = image_path.split("/")
-        img_filename = img_path_list[-1]
-        img_folder = img_path_list[-2]
-        img_dir = "/".join(img_path_list[:-2])
-        name = ".".join(img_filename.split(".")[:-1]) + self.MASKS_FILENAME_TAG
-        mask_filename = name + self.MASK_EXTENSION
-        mask_folder = img_folder.split("_")[0] + self.MASKS_DIR_TAG
-        return os.path.join(img_dir, mask_folder, mask_filename)
+class Casia1CopyMoveOSNDataset(CasiaBaseDataset):
+    """
+    Class for the CASIA 1.0 Copy Move subset with Online Social Networks (OSN)
+    modifications.
 
-    def _binarize_mask(self, mask_image: Tensor) -> Tensor:
-        return mask_image[0, :, :] > self.TAMPERED_INTENSITY_THRESHOLD
+    Directory structure:
+    img_dir (OSN dataset)
+    ├── Casia_Facebook
+    │   ├── CM
+    │   │   └── [Copy Move images in jpg]
+    │   ├── Sp
+    │   │   └── [Spliced images in jpg]
+    │   ├── CASIA_GT
+    │       └── [Copy Move and Spliced masks in png]
+    └── Possibly more files
+    """
+
+    IMAGES_TAMPERED_DIR = "Casia_Facebook/CM"
+    MASK_TAMPERED_DIR = "Casia_Facebook/CASIA_GT"
