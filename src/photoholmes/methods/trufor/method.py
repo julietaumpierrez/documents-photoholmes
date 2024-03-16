00ea521cf07d6844c593ea94b7dc51ca4da8dd3b
Julieta Umpierrez <julietaumpierrez@192.168.1.3>
2024-03-15 23:53:19 -0300

    Review trufor

--- src/photoholmes/methods/trufor/method.py
+++ src/photoholmes/methods/trufor/method.py
@@ -336,8 +336,10 @@ class TruFor(BaseTorchMethod):
     def benchmark(self, image: Tensor) -> BenchmarkOutput:
         """
         Benchmarks the TruFor method using the provided image.
+
         Args:
             image (Tensor): Input image tensor.
+
         Returns:
             BenchmarkOutput: Contains the heatmap and detection and placeholder for mask.
         """
